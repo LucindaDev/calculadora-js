3a952fa5e1e190aeb6121086becd9790735ccf08
LucindaDev <lucindadsanchez@gmail.com>
2024-11-06 11:31:20 -0700

    Refactor correction.

--- index.js
+++ index.js
@@ -16,57 +16,69 @@ buttons.forEach(button => {
             } catch (e) {
                 console.log(e);
                 screen.textContent = "Error!";
+
+                switchButtons(true,"btn-c","btn-ce");
             }
 
             return;
-        }
+        };
 
         //#region Btn C para reiniciar.
         if (button.id === "btn-c") {
             screen.textContent = "0";
 
-            switchButtons(false);
+            switchButtons(false,"btn-c");
 
             return;
         };
 
         //#region Btn CE para borrar.
         if (button.id === "btn-ce") {
-            if (screen.textContent.length === 1 || screen.textContent === "Error!") {
+            if (screen.textContent.length === 1 || screen.textContent === "Error!" || screen.textContent === "EEEE") {
                 screen.textContent = "0";
+                switchButtons(false);
             } else {
                 screen.textContent = screen.textContent.slice(0, -1);
             }
             return;
-        }
+        };
 
 
         //#region Estado inicial y agregar dígitos.
-        if (screen.textContent === "0") {
-            screen.textContent = btnSelected;
-        } else {
-            if (isResult == true) {
-                screen.textContent = btnSelected;
-                isResult = false;
-            } else {
-                screen.textContent += btnSelected;
-            }
-        }
+        ingresarDigitos(btnSelected);
 
         //#region Validación límite de dígitos.
         if (screen.textContent.length > 10) {
             screen.textContent = "EEEE"
 
-            if (button.id !== "btn-c") {
-                switchButtons(true);
-            }
-        }
+                switchButtons(true,"btn-c","btn-ce");
+        };
 
-    })
+    });
 });
 
-const switchButtons = (state) => {
+const switchButtons = (state, ignoreBtn1, ignoreBtn2) => {
     buttons.forEach(button => {
+        if(button.id === ignoreBtn1){
+            return;
+        }
+        if(button.id === ignoreBtn2){
+            return;
+        }
+        
         button.disabled = state;
     });
+};
+
+const ingresarDigitos = (btnSelected) => {
+    if (screen.textContent === "0") {
+        screen.textContent = btnSelected;
+    } else {
+        if (isResult == true) {
+            screen.textContent = btnSelected;
+            isResult = false;
+        } else {
+            screen.textContent += btnSelected;
+        }
+    };
 }
